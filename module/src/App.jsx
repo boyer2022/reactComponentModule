0409c09a45f0f179baf2c181752ad6be1c52e63e
Matthew Boyer <lyfsux@MacBookPro.lan>
2025-07-30 14:42:31 -0500

    Initial setup. Created button, no styles

---
+++ module/src/App.jsx
@@ -0,0 +1,19 @@
+//  How to style React Components with css
+// ---------------------------------------
+// 1. External
+// 2. MODULES
+// 3. Inline
+
+import Button from './Button.jsx'
+
+function App() {
+  
+
+  return (
+    <Button />
+      
+   
+  )
+}
+
+export default App
---
+++ module/src/Button.jsx
@@ -0,0 +1,6 @@
+
+function Button(){
+
+    return(<button className="button">CLICK ME</button>);
+}
+ export default Button
